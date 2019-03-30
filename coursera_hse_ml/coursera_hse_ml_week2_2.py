bb16423f377ae39987c622c9a79de13e9da9105a
pk0d <pkod@bk.ru>
2019-03-30 16:33:28 +0300

    Решение задачи 2 второй недели курса по мл

--- coursera_hse_ml/coursera_hse_ml_week2_2.py
+++ coursera_hse_ml/coursera_hse_ml_week2_2.py
@@ -1,5 +1,25 @@
 import pandas as pd
+from sklearn.linear_model import Perceptron
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import StandardScaler
 
-train = read_csv('perceptron-train.csv')
-test = read_csv('perceptron-test.csv')
+X_train = pd.read_csv('perceptron-train.csv', header=None).ix[:, 1:]
+X_test = pd.read_csv('perceptron-test.csv', header=None).ix[:, 1:]
+y_train = pd.read_csv('perceptron-train.csv', header=None).ix[:, 0]
+y_test = pd.read_csv('perceptron-test.csv', header=None).ix[:, 0]
 
+model = Perceptron(random_state=241)
+model.fit(X_train, y_train)
+acc_before = accuracy_score(y_test, model.predict(X_test))
+print(acc_before)
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+model = Perceptron(random_state=241)
+model.fit(X_train_scaled, y_train)
+#predict_train=model.predict(X_train_scaled)
+predict_test=model.predict(X_test_scaled)
+
+acc_after = accuracy_score(y_test, predict_test)
+print(acc_after)
+print(acc_after - acc_before)
